399feabc9ac46ca0936c7a17ede81d4fffac7134
Gabriel Arias <gabriel.arias@edificiosanluis.com>
2025-12-18 20:56:08 -0500

    Implement Basic Cli + Add Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+goa: src/main.c
+	gcc src/main.c -o goa
--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
-#include <stdio.h>  
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
-int main() {
-    printf("Hello World!\n");
+int main(int argc, char **argv) {
+
+    // TODO: add -o flag to customize output file name
+    if (argc < 2) {
+        printf("goa: error: no input files\n");
+        return 1;
+    } else if (argc > 2) {
+        printf("goa: error: too many arguments\n");
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        perror("goa");
+        return 1;
+    }
+
+    // Compile and stuff
+
+    if(fclose(fp) != 0) {
+        perror("goa");
+        return 1;
+    }
     return 0;
 }
